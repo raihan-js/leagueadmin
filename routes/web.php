53c5dac4d3d98edcfeeef271a81cd5c68e4c5c19
raihan-js <araihansikder@gmail.com>
2023-02-06 22:49:13 +0600

    Controller name fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthController;
-use App\Http\Controllers\Admin\AdminContoller;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AdminPageController;
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\Admin\RoleController;
@@ -24,7 +24,7 @@ Route::group([ 'middleware' => 'admin' ], function(){
     // Pages
     Route::resource('permissions', PermissionController::class);
     Route::resource('roles', RoleController::class);
-    Route::resource('admins', AdminContoller::class);
+    Route::resource('admins', AdminController::class);
 });
 
 
